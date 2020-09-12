dd3cc414234dd07c1870e24902a8f192c83a7387
ssolid20 <61514794+ssolid20@users.noreply.github.com>
2020-09-12 17:51:01 +0200

    Update Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -344,7 +344,7 @@ methods: {
                 this.showAnswerPart3("Couldn't regonise problem or there are no solutions, try again")}
             else {
             let r  = response.data
-            //r = math.round(r,5)
+            r = math.round(r,7)
             this.showAnswerPart2(r)
             }
             }).catch((err) => {
